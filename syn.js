04b93d80f1c9c70fc2ab4cc1193a481a1038a81f
awesomedude939 <96753200+awesomedude939@users.noreply.github.com>
2022-09-07 19:57:07 -0300

    Create syn.js

---
+++ syn.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Fix Synapse X Website
+// @namespace    https://x.synapse.to/
+// @version      0.1
+// @description  none
+// @author       ilik
+// @match        https://x.synapse.to/
+// @icon         https://external-content.duckduckgo.com/ip3/x.synapse.to.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.getElementById("site-fundraising").remove()
+})();
