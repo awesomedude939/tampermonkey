120255a381538b2941b01ddd710beff3a4f06b37
awesomedude939 <96753200+awesomedude939@users.noreply.github.com>
2022-09-07 20:00:40 -0300

    Update syn.js

--- syn.js
+++ syn.js
@@ -2,11 +2,13 @@
 // @name         Fix Synapse X Website
 // @namespace    https://x.synapse.to/
 // @version      0.1
-// @description  none
+// @description  Remove useless
 // @author       ilik
 // @match        https://x.synapse.to/
 // @icon         https://external-content.duckduckgo.com/ip3/x.synapse.to.ico
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/awesomedude939/tampermonkey/main/syn.js
+// @downloadURL  https://raw.githubusercontent.com/awesomedude939/tampermonkey/main/syn.js
 // ==/UserScript==
 
 (function() {
